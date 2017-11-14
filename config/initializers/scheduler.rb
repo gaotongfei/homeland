5509041d42e64bee0e93ba9859cd3a81dcd56d31
Taff Gao <gaotongfei1995@gmail.com>
2017-11-14 22:57:00 +0800

    abstract topic score calculation

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,36 +1,26 @@
 require 'rufus/scheduler'
+require 'topic_score'
+
+include Homeland
 
 scheduler = Rufus::Scheduler.new
 
 # update weekly trending every 1 hour
-scheduler.every '5s' do
+scheduler.every '1h' do
   topics = Topic.all
   topics.map do |topic|
-    score = 0
-    reply_score = 0
-    view_score = 0
-    replies = topic.replies
-    (1..7).each do |day|
-      # calculate replies score
-      replies.each do |reply|
-        if Time.now - reply.created_at < day.send(:days)
-          reply_score += 1
-        end
-      end
-
-      # calculate topic views score
-      topic.hit_at.each do |hit|
-        if Time.now - Time.parse(hit) < day.send(:days)
-          view_score += 1
-        end
-      end
-    end
-    score = 3 * reply_score + view_score
+    ts = TopicScore.new
+    score = ts.score(7, :days, topic)
     topic.update(weekly_score: score)
   end
 end
 
-
 # update daily trending every 10 minutes
 scheduler.every '10m' do
+  topics = Topic.all
+  topics.map do |topic|
+    ts = TopicScore.new
+    score = ts.score(24, :hours, topic)
+    topic.update(daily_score: score)
+  end
 end
--- lib/homeland/topic_score.rb
+++ lib/homeland/topic_score.rb
@@ -1,4 +1,33 @@
 module Homeland
   class TopicScore
+    # ts = TopicScore.new
+    # topics = Topic.all
+    # ts.score(7, :days, topics)
+    def initialize
+      @reply_score = 0
+      @view_score = 0
+      @score = 0
+    end
+
+    def score(counts, unit, topic, options={})
+      default_options = {start: 1}
+      options = default_options.merge(options)
+      start = options[:start]
+      (start..counts).each do |u|
+        topic.replies.each do |reply|
+          if Time.now - reply.created_at < u.send(unit.to_sym)
+            @reply_score += 1
+          end
+        end
+
+        topic.hit_at.each do |hit|
+          if Time.now - Time.parse(hit) < u.send(unit.to_sym)
+            @view_score += 1
+          end
+        end
+      end
+      @score = 3 * @reply_score + @view_score
+    end
+
   end
 end
